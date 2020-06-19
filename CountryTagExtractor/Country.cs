81251ebcda8b1101b4295d0c38ea8ffa9d5ad9ef
vitalia-kontanistova <v.konti@outlook.com>
2020-06-20 01:29:26 +0300

    Добавьте файлы проекта.

---
+++ CountryTagExtractor/Country.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountryTagExtractor
+{
+    class Country
+    {
+        public string Name { get; set; }
+        public string[] Cities { get; set; }
+
+        public Dictionary<string, int> CitiesMentions { get; } = new Dictionary<string, int>();
+        public Dictionary<string, int> CountryMentions { get; } = new Dictionary<string, int>();
+
+        public int Coefficient { get; set; } = 0;
+
+        public Country(string name, string[] cities)
+        {
+            Name = name;
+            Cities = cities;
+        }
+
+        public void СalculateСoefficient()
+        {
+            foreach (int number in CitiesMentions.Values)
+                Coefficient += number;
+            foreach (int number in CountryMentions.Values)
+                Coefficient += number;
+
+            //if (CountryMentions.Count > 0)
+            //    Coefficient += CountryMentions[Name];
+        }
+    }
+}
---
+++ CountryTagExtractor/HtmlLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryTagExtractor
+{
+    static class HtmlLoader
+    {
+        static private HttpClient client;
+        static string source = null;
+
+        public static async Task<string> LoadAsync(string URL)
+        {
+            client = new HttpClient();
+
+            var response = await client.GetAsync(URL);
+
+            if (response != null && response.StatusCode == HttpStatusCode.OK)
+            {
+                source = await response.Content.ReadAsStringAsync();
+            }
+
+            return source;
+        }
+    }
+}
